5c8eb0175b1316eb674bbdabf10e8fe4bea58cee
mellenpan <panellen1106@gmail.com>
2026-09-16 22:59:10 -0400

    MSE train .106

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -1,6 +1,7 @@
 # packages
 import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
+print(pd.__version__)
 
 # set seed
 seed = 314
@@ -10,12 +11,13 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
-        max_depth=8,
-        subsample=1,
-        min_samples_leaf=1,
+        learning_rate=0.05,
+        n_estimators=150,
+        max_depth=2,
+        subsample=.8,
+        min_samples_leaf=10,
         random_state=seed
+    
     )
     model.fit(X, y)
     return model
